1a7d54132ce70516ada8dad673378d8c0cf42331
Seyed Amir Mahdavi <seyedamir_mahdavi@yahoo.com>
2018-09-26 15:11:42 +0330

    it's worked

--- shorten.pipe.ts
+++ shorten.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
   transform(value: any, args?: any): any {
+    // if your string length is bigger than args this pipe shorten your string and add (...) at the end of string
     if (value.length > args) {
       return value.substr(0, args) + ' ...';
     }
